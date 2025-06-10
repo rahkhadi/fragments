d6933db2bdb634e98dc02555a1d53d748871198c
Rahkhadim <rkhadim-hussain@myseneca.ca>
2025-06-09 23:29:32 -0400

    Fix ESLint warning in rawBody.js by removing unused variable

--- src/middleware/rawBody.js
+++ src/middleware/rawBody.js
@@ -12,7 +12,7 @@ const rawBody = () =>
       try {
         const { type } = contentType.parse(req);
         return Fragment.isSupportedType(type);
-      } catch (err) {
+      } catch {
         return false;
       }
     },
