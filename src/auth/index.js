8f65b5b62061f24236af7923fbea24b6f8e970b2
Rahkhadim <rkhadim-hussain@myseneca.ca>
2025-07-10 11:47:02 -0400

    done

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,9 +1,11 @@
-// src/auth/index.js
-
 // Make sure our env isn't configured for both AWS Cognito and HTTP Basic Auth.
 // We can only do one or the other.  If your .env file contains all 3 of these
 // variables, something is wrong.  It should have AWS_COGNITO_POOL_ID and
 // AWS_COGNITO_CLIENT_ID together OR HTPASSWD_FILE on its own.
+
+const logger = require('../logger');
+let authModule;
+
 if (
   process.env.AWS_COGNITO_POOL_ID &&
   process.env.AWS_COGNITO_CLIENT_ID &&
@@ -15,9 +17,14 @@ if (
 }
 
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
-  module.exports = require('./cognito');
+  logger.info('Auth index: Using AWS Cognito strategy');
+  authModule = require('./cognito');
 } else if (process.env.HTPASSWD_FILE && process.env.NODE_ENV !== 'production') {
-  module.exports = require('./basic-auth');
+  logger.info('Auth index: Using HTTP Basic Auth strategy');
+  authModule = require('./basic-auth');
 } else {
   throw new Error('missing env vars: no authorization configuration found');
 }
+
+module.exports.strategy = authModule.strategy;
+module.exports.authenticate = authModule.authenticate;
