d252738b4d15d0ebb05e121557bc2b47ec68a038
Rahkhadim <rkhadim-hussain@myseneca.ca>
2025-08-04 14:43:25 -0400

    Switch data model to S3/DynamoDB integration

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,4 +1,30 @@
 // src/model/data/index.js
 
 const useAws = process.env.FRAGMENTS_DB_TYPE === 'aws';
-module.exports = useAws ? require('./aws') : require('./memory-db');
+
+if (useAws) {
+  module.exports = require('./aws');
+} else {
+  // Wrap memory-db with exports to match aws module API
+  const MemoryDB = require('./memory-db');
+  const db = new MemoryDB();
+
+  module.exports = {
+    // Metadata
+    writeFragment: (fragment) => db.put(fragment.ownerId, fragment.id, fragment),
+    readFragment: (ownerId, id) => db.get(ownerId, id),
+    deleteFragment: async (ownerId, id) => {
+      await db.del(ownerId, id);
+      return db.del(ownerId, `${id}:data`);
+    },
+    listFragments: async (ownerId, expand = false) => {
+      const all = await db.query(ownerId);
+      return all.map((f) => (expand ? f : f.id)).filter(Boolean);
+    },
+
+    // Data
+    writeFragmentData: (ownerId, id, buffer) => db.put(ownerId, `${id}:data`, buffer),
+    readFragmentData: (ownerId, id) => db.get(ownerId, `${id}:data`),
+    deleteFragmentData: (ownerId, id) => db.del(ownerId, `${id}:data`)
+  };
+}
