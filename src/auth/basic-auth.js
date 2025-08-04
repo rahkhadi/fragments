d0560377db1b69f9fe428be92e3dc3c8ab97acbe
Rahkhadim <rkhadim-hussain@myseneca.ca>
2025-08-04 14:43:24 -0400

    Improve basic auth handling and logging

--- src/auth/basic-auth.js
+++ src/auth/basic-auth.js
@@ -1,27 +1,19 @@
-// src/auth/basic-auth.js
-
-// Configure HTTP Basic Auth strategy for Passport, see:
-// https://github.com/http-auth/http-auth-passport
-
 const auth = require('http-auth');
 const passport = require('passport');
 const authPassport = require('http-auth-passport');
 const logger = require('../logger');
+const path = require('path'); // ✅ you're already importing it here
 
-// We expect HTPASSWD_FILE to be defined.
 if (!process.env.HTPASSWD_FILE) {
   throw new Error('missing expected env var: HTPASSWD_FILE');
 }
 
-// Log that we're using Basic Auth
 logger.info('Using HTTP Basic Auth for auth');
 
 module.exports.strategy = () =>
-  // For our Passport authentication strategy, we'll look for a
-  // username/password pair in the Authorization header.
   authPassport(
     auth.basic({
-      file: process.env.HTPASSWD_FILE,
+      file: path.resolve(process.env.HTPASSWD_FILE), // ✅ use `path` directly
     })
   );
 
