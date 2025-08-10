dbe046f796e7fc409b69403c5ffdd9553f4bb2b9
Rahkhadim <rkhadim-hussain@myseneca.ca>
2025-08-10 02:28:36 -0400

    Update API index to include AWS integrated routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,26 +3,26 @@ const express = require('express');
 const router = express.Router();
 
 const rawBody = require('../../middleware/rawBody');
-const authenticate = require('../../auth/auth-middleware');
+const auth = require('../../auth');
 
 const postRoute = require('./post');
+const putRoute  = require('./put');
 const get = require('./get');
 const getById = require('./get-id');
 const getInfo = require('./get-info');
 const getExtension = require('./get-extension');
 const del = require('./delete');
 
-// All fragment routes require authentication
-router.use('/fragments', authenticate('http'));
-
-// Apply rawBody to POST only
+router.use('/fragments', auth.authenticate());
 router.use('/fragments', rawBody(), postRoute);
+router.use('/fragments', rawBody(), putRoute);   // put.js path should be: router.put('/:id', ...)
 
-// Other routes
+// Other routes (order matters!)
 router.get('/fragments', get);
-router.get('/fragments/:id', getById);
+router.get('/fragments/:id.:ext', getExtension);  
 router.get('/fragments/:id/info', getInfo);
-router.get('/fragments/:id.:ext', getExtension);
+router.get('/fragments/:id', getById);            
 router.delete('/fragments/:id', del);
 
 module.exports = router;
+
